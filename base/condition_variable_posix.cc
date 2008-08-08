f5a40001b9d5fab5a18fd4d03e02ed9fc78bf07a
mmentovai@google.com <mmentovai@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-09 01:19:43 +0900

    Port LockImpl, Lock, and ConditionVariable to pthreads-supporting platforms.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@567 0039d316-1c4b-4281-b951-d872f2087c98
    
    
    CrOS-Libchrome-Original-Commit: 08048c71997f7ec047f20fb0132db3884bada97d

---
+++ base/condition_variable_posix.cc
@@ -0,0 +1,85 @@
+// Copyright 2008, Google Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//    * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//    * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "base/condition_variable.h"
+
+#include <errno.h>
+#include <sys/time.h>
+
+#include "base/lock.h"
+#include "base/lock_impl.h"
+#include "base/logging.h"
+
+ConditionVariable::ConditionVariable(Lock* user_lock)
+    : user_mutex_(user_lock->lock_impl()->os_lock()) {
+  int rv = pthread_cond_init(&condition_, NULL);
+  DCHECK(rv == 0);
+}
+
+ConditionVariable::~ConditionVariable() {
+  int rv = pthread_cond_destroy(&condition_);
+  DCHECK(rv == 0);
+}
+
+void ConditionVariable::Wait() {
+  int rv = pthread_cond_wait(&condition_, user_mutex_);
+  DCHECK(rv == 0);
+}
+
+void ConditionVariable::TimedWait(const TimeDelta& max_time) {
+  int64 usecs = max_time.InMicroseconds();
+
+  // The timeout argument to pthread_cond_timedwait is in absolute time.
+  struct timeval now;
+  gettimeofday(&now, NULL);
+
+  const int kMicrosPerUnit = 1000000;
+  const int kNanosPerMicro = 1000;
+  const int kNanosPerUnit = kMicrosPerUnit * kNanosPerMicro;
+
+  struct timespec abstime;
+  abstime.tv_sec = now.tv_sec + (usecs / kMicrosPerUnit);
+  abstime.tv_nsec = (now.tv_usec + (usecs % kMicrosPerUnit)) * kNanosPerMicro;
+  abstime.tv_sec += abstime.tv_nsec / kNanosPerUnit;
+  abstime.tv_nsec %= kNanosPerUnit;
+  DCHECK(abstime.tv_sec >= now.tv_sec);  // Overflow paranoia
+
+  int rv = pthread_cond_timedwait(&condition_, user_mutex_, &abstime);
+  DCHECK(rv == 0 || rv == ETIMEDOUT);
+}
+
+void ConditionVariable::Broadcast() {
+  int rv = pthread_cond_broadcast(&condition_);
+  DCHECK(rv == 0);
+}
+
+void ConditionVariable::Signal() {
+  int rv = pthread_cond_signal(&condition_);
+  DCHECK(rv == 0);
+}
--- base/lock_impl_posix.cc
+++ base/lock_impl_posix.cc
@@ -27,70 +27,34 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef BASE_LOCK_IMPL_H_
-#define BASE_LOCK_IMPL_H_
+#include "base/lock_impl.h"
 
-#include "build/build_config.h"
+#include <errno.h>
 
-#if defined(OS_WIN)
-#include <windows.h>
-#elif defined(OS_POSIX)
-#include <pthread.h>
-#endif
+#include "base/logging.h"
 
-#include "base/basictypes.h"
+LockImpl::LockImpl() {
+  int rv = pthread_mutex_init(&os_lock_, NULL);
+  DCHECK(rv == 0);
+}
 
-// This class implements the underlying platform-specific spin-lock mechanism
-// used for the Lock class.  Most users should not use LockImpl directly, but
-// should instead use Lock.
-class LockImpl {
- public:
-#if defined(OS_WIN)
-  typedef CRITICAL_SECTION OSLockType;
-#elif defined(OS_POSIX)
-  typedef pthread_mutex_t OSLockType;
-#endif
+LockImpl::~LockImpl() {
+  int rv = pthread_mutex_destroy(&os_lock_);
+  DCHECK(rv == 0);
+}
 
-  LockImpl();
-  ~LockImpl();
+bool LockImpl::Try() {
+  int rv = pthread_mutex_trylock(&os_lock_);
+  DCHECK(rv == 0 || rv == EBUSY);
+  return rv == 0;
+}
 
-  // If the lock is not held, take it and return true.  If the lock is already
-  // held by something else, immediately return false.
-  bool Try();
+void LockImpl::Lock() {
+  int rv = pthread_mutex_lock(&os_lock_);
+  DCHECK(rv == 0);
+}
 
-  // Take the lock, blocking until it is available if necessary.
-  void Lock();
-
-  // Release the lock.  This must only be called by the lock's holder: after
-  // a successful call to Try, or a call to Lock.
-  void Unlock();
-
-  // Return the native underlying lock.
-  // TODO(awalker): refactor lock and condition variables so that this is
-  // unnecessary.
-  OSLockType* os_lock() { return &os_lock_; }
-
- private:
-  OSLockType os_lock_;
-
-  DISALLOW_COPY_AND_ASSIGN(LockImpl);
-};
-
-class AutoLockImpl {
- public:
-  AutoLockImpl(LockImpl* lock_impl)
-      : lock_impl_(lock_impl) {
-    lock_impl_->Lock();
-  }
-
-  ~AutoLockImpl() {
-    lock_impl_->Unlock();
-  }
-
- private:
-  DISALLOW_EVIL_CONSTRUCTORS(AutoLockImpl);
-
-  LockImpl* lock_impl_;
-};
-
-#endif  // BASE_LOCK_IMPL_H_
+void LockImpl::Unlock() {
+  int rv = pthread_mutex_unlock(&os_lock_);
+  DCHECK(rv == 0);
+}
