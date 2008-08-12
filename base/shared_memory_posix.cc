3a20898b66f3f7cddcbeb2f320393545179705ed
avi@google.com <avi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-13 06:06:40 +0900

    The Posix shared memory implementation.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@742 0039d316-1c4b-4281-b951-d872f2087c98
    
    
    CrOS-Libchrome-Original-Commit: 4fc77b95e324c437a0be79a3d4244eddac9f36e3

--- base/shared_memory_posix.cc
+++ base/shared_memory_posix.cc
@@ -29,11 +29,19 @@
 
 #include "base/shared_memory.h"
 
+#include <sys/mman.h>
+
 #include "base/logging.h"
-#include "base/win_util.h"
+#include "base/string_util.h"
+
+namespace {
+  // Paranoia. Semaphores and shared memory segments should live in different
+  // namespaces, but who knows what's out there.
+  const char kSemaphoreSuffix[] = "-sem";
+}
 
 SharedMemory::SharedMemory()
-    : mapped_file_(NULL),
+    : mapped_file_(-1),
       memory_(NULL),
       read_only_(false),
       max_size_(0),
@@ -50,78 +58,91 @@ SharedMemory::SharedMemory(SharedMemoryHandle handle, bool read_only)
 
 SharedMemory::SharedMemory(SharedMemoryHandle handle, bool read_only,
                            ProcessHandle process)
-    : mapped_file_(NULL),
+    : mapped_file_(handle),
       memory_(NULL),
       read_only_(read_only),
       max_size_(0),
       lock_(NULL) {
-  ::DuplicateHandle(process, handle,
-                    GetCurrentProcess(), &mapped_file_,
-                    STANDARD_RIGHTS_REQUIRED |
-                    (read_only_ ? FILE_MAP_READ : FILE_MAP_ALL_ACCESS),
-                    FALSE, 0);
+  // We don't handle this case yet (note the ignored parameter); let's die if
+  // someone comes calling.
+  NOTREACHED();
 }
 
 SharedMemory::~SharedMemory() {
   Close();
   if (lock_ != NULL)
-    CloseHandle(lock_);
+    sem_close(lock_);
 }
 
 bool SharedMemory::Create(const std::wstring &name, bool read_only,
                           bool open_existing, size_t size) {
-  DCHECK(mapped_file_ == NULL);
+  DCHECK(mapped_file_ == -1);
 
-  name_ = name;
+  name_ = L"/" + name;
   read_only_ = read_only;
-  mapped_file_ = CreateFileMapping(INVALID_HANDLE_VALUE, NULL,
-      read_only_ ? PAGE_READONLY : PAGE_READWRITE, 0, static_cast<DWORD>(size),
-      name.empty() ? NULL : name.c_str());
-  if (!mapped_file_)
-    return false;
 
-  // Check if the shared memory pre-exists.
-  if (GetLastError() == ERROR_ALREADY_EXISTS && !open_existing) {
-    Close();
+  int posix_flags = 0;
+  posix_flags |= read_only ? O_RDONLY : O_RDWR;
+  posix_flags |= O_CREAT;
+  
+  int posix_mode = 0600;  // owner read/write
+  std::string posix_name(WideToUTF8(name_));
+  mapped_file_ = shm_open(posix_name.c_str(), posix_flags, posix_mode);
+  
+  if (mapped_file_ < 0)
+    return false;
+  ftruncate(mapped_file_, size);
+  
+  posix_name += kSemaphoreSuffix;
+  lock_ = sem_open(posix_name.c_str(), O_CREAT, posix_mode, 1);
+  if (lock_ == SEM_FAILED) {
+    close(mapped_file_);
+    mapped_file_ = -1;
+    shm_unlink(posix_name.c_str());
+    lock_ = NULL;
     return false;
   }
+
   max_size_ = size;
   return true;
 }
 
 bool SharedMemory::Open(const std::wstring &name, bool read_only) {
-  DCHECK(mapped_file_ == NULL);
+  DCHECK(mapped_file_ == -1);
 
   name_ = name;
   read_only_ = read_only;
-  mapped_file_ = OpenFileMapping(
-      read_only_ ? FILE_MAP_READ : FILE_MAP_ALL_ACCESS, false,
-      name.empty() ? NULL : name.c_str());
-  if (mapped_file_ != NULL) {
-    // Note: size_ is not set in this case.
-    return true;
-  }
-  return false;
+  
+  int posix_flags = 0;
+  posix_flags |= read_only ? O_RDONLY : O_RDWR;
+  
+  int posix_mode = 0600;  // owner read/write
+  std::string posix_name(WideToUTF8(name_));
+  mapped_file_ = shm_open(posix_name.c_str(), posix_flags, posix_mode);
+  
+  return (mapped_file_ >= 0);
 }
 
 bool SharedMemory::Map(size_t bytes) {
-  if (mapped_file_ == NULL)
+  if (mapped_file_ == -1)
     return false;
+  
+  memory_ = mmap(NULL, bytes, PROT_READ | (read_only_ ? 0 : PROT_WRITE),
+                 MAP_SHARED, mapped_file_, 0);
 
-  memory_ = MapViewOfFile(mapped_file_,
-      read_only_ ? FILE_MAP_READ : FILE_MAP_ALL_ACCESS, 0, 0, bytes);
-  if (memory_ != NULL) {
-    return true;
-  }
-  return false;
+  if (memory_)
+    max_size_ = bytes;
+
+  return (memory_ != NULL);
 }
 
 bool SharedMemory::Unmap() {
   if (memory_ == NULL)
     return false;
 
-  UnmapViewOfFile(memory_);
+  munmap(memory_, max_size_);
   memory_ = NULL;
+  max_size_ = 0;
   return true;
 }
 
@@ -129,59 +150,40 @@ bool SharedMemory::ShareToProcessCommon(ProcessHandle process,
                                         SharedMemoryHandle *new_handle,
                                         bool close_self) {
   *new_handle = 0;
-  DWORD access = STANDARD_RIGHTS_REQUIRED | FILE_MAP_READ;
-  DWORD options = 0;
-  HANDLE mapped_file = mapped_file_;
-  HANDLE result;
-  if (!read_only_)
-    access |= FILE_MAP_WRITE;
-  if (close_self) {
-    // DUPLICATE_CLOSE_SOURCE causes DuplicateHandle to close mapped_file.
-    options = DUPLICATE_CLOSE_SOURCE;
-    mapped_file_ = NULL;
-    Unmap();
-  }
-
-  if (process == GetCurrentProcess() && close_self) {
-    *new_handle = mapped_file;
-    return true;
-  }
-
-  if (!DuplicateHandle(GetCurrentProcess(), mapped_file, process,
-      &result, access, FALSE, options))
-    return false;
-  *new_handle = result;
-  return true;
+  // TODO(awalker): figure out if we need this, and do the appropriate
+  // VM magic if so.
+  return false;
 }
 
 
 void SharedMemory::Close() {
   if (memory_ != NULL) {
-    UnmapViewOfFile(memory_);
+    munmap(memory_, max_size_);
+
     memory_ = NULL;
+    max_size_ = 0;
   }
 
-  if (mapped_file_ != NULL) {
-    CloseHandle(mapped_file_);
-    mapped_file_ = NULL;
+  std::string posix_name(WideToUTF8(name_));
+  if (mapped_file_) {
+    close(mapped_file_);
+    shm_unlink(posix_name.c_str());
+    mapped_file_ = -1;
+  }
+  
+  if (lock_) {
+    posix_name += kSemaphoreSuffix;
+    sem_unlink(posix_name.c_str());
+    lock_ = NULL;
   }
 }
 
 void SharedMemory::Lock() {
-  if (lock_ == NULL) {
-    std::wstring name = name_;
-    name.append(L"lock");
-    lock_ = CreateMutex(NULL, FALSE, name.c_str());
-    DCHECK(lock_ != NULL);
-    if (lock_ == NULL) {
-      DLOG(ERROR) << "Could not create mutex" << GetLastError();
-      return;  // there is nothing good we can do here.
-    }
-  }
-  WaitForSingleObject(lock_, INFINITE);
+  DCHECK(lock_ != NULL);
+  sem_wait(lock_);
 }
 
 void SharedMemory::Unlock() {
   DCHECK(lock_ != NULL);
-  ReleaseMutex(lock_);
+  sem_post(lock_);
 }
