9e746b501ae9a257caf114ab02be8a734e4cf510
Ari Hausman-Cohen <arihc@google.com>
2015-09-30 21:56:34 +0000

    Revert "Adding module tags so tests get built by default."
    This reverts commit 369474b61aa9b100057bb6b174241ea45fafcdb4.
    
    Change-Id: I267027c0ca9808832e7a6837ec41c1de1885adf8

--- Android.mk
+++ Android.mk
@@ -492,7 +492,6 @@ include $(BUILD_HOST_STATIC_LIBRARY)
 # ========================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE := libchrome-host_test
-LOCAL_MODULE_TAGS := debug
 LOCAL_SRC_FILES := $(libchromeCommonUnittestSrc)
 LOCAL_RTTI_FLAG := -frtti
 LOCAL_CPP_EXTENSION := $(libchromeCommonCppExtension)
@@ -510,7 +509,6 @@ endif
 # ========================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE := libchrome_test
-LOCAL_MODULE_TAGS := debug
 LOCAL_SRC_FILES := $(libchromeCommonUnittestSrc)
 LOCAL_RTTI_FLAG := -frtti
 LOCAL_CPP_EXTENSION := $(libchromeCommonCppExtension)
