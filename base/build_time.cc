a3f0ea1b5747b2bdc0fe4df8685162d6174fe362
Dan Willemsen <dwillemsen@google.com>
2015-10-29 10:11:23 +0900

    Don't use __DATE__/__TIME__ on Android
    Instead, pull the build date of the system from the ro.build.date system
    property. Then this library will be identical as long as the sources and
    dependencies don't change, and we won't have to update it on every OTA.
    
    Bug: 24204119
    Change-Id: Ie5368ec0bbbc635dc6b86f9259d6567fe26ca2ba
    
    (Reconstructed from 8b7977eccc94f6b3a3896cd13b4aeacbfa1e0f84)

--- base/build_time.cc
+++ base/build_time.cc
@@ -11,11 +11,17 @@
 #include "base/time/time.h"
 
 #if defined(OS_ANDROID)
+#include <cutils/properties.h>
+#endif
+
 namespace base {
 
 Time GetBuildTime() {
   Time integral_build_time;
 #if defined(OS_ANDROID)
+  char kDateTime[PROPERTY_VALUE_MAX];
+  property_get("ro.build.date", kDateTime, "Sep 02 2008 08:00:00 PST");
+#elif defined(DONT_EMBED_BUILD_METADATA) && !defined(OFFICIAL_BUILD)
   DCHECK(result);
   return integral_build_time;
 }
