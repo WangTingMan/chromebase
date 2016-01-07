da04820d4f6c808cc997a3957be0aad62c4f3fd2
Bertrand SIMONNET <bsimonnet@google.com>
2016-01-06 17:45:01 -0800

    ChromeOS: Don't abort when the dbus connection drops.
    We don't abort when the dbus bus disconnects on Android, we should do
    the same on Chrome OS.
    
    Bug: 26267381
    Change-Id: I7b4060e91c89916fdaca33a617cdcae6c98e21e3

--- dbus/bus.cc
+++ dbus/bus.cc
@@ -23,13 +23,6 @@ namespace dbus {
 
 namespace {
 
-#if !defined(__ANDROID__)
-const char kDisconnectedSignal[] = "Disconnected";
-const char kDisconnectedMatchRule[] =
-    "type='signal', path='/org/freedesktop/DBus/Local',"
-    "interface='org.freedesktop.DBus.Local', member='Disconnected'";
-#endif
-
 // The NameOwnerChanged member in org.freedesktop.DBus
 const char kNameOwnerChangedSignal[] = "NameOwnerChanged";
 
@@ -447,15 +440,6 @@ bool Bus::Connect() {
     }
   }
 
-#if !defined(__ANDROID__)
-  // We shouldn't exit on the disconnected signal.
-  dbus_connection_set_exit_on_disconnect(connection_, false);
-
-  // Watch Disconnected signal.
-  AddFilterFunction(Bus::OnConnectionDisconnectedFilter, this);
-  AddMatch(kDisconnectedMatchRule, error.get());
-#endif
-
   return true;
 }
 
@@ -514,11 +498,6 @@ void Bus::ShutdownAndBlock() {
   if (connection_) {
     // Remove Disconnected watcher.
     ScopedDBusError error;
-
-#if !defined(__ANDROID__)
-    RemoveFilterFunction(Bus::OnConnectionDisconnectedFilter, this);
-    RemoveMatch(kDisconnectedMatchRule, error.get());
-#endif
 
     if (connection_type_ == PRIVATE)
       ClosePrivateConnection();
@@ -1194,22 +1173,6 @@ void Bus::OnDispatchStatusChangedThunk(DBusConnection* connection,
   Bus* self = static_cast<Bus*>(data);
   self->OnDispatchStatusChanged(connection, status);
 }
-
-#if !defined(__ANDROID__)
-// static
-DBusHandlerResult Bus::OnConnectionDisconnectedFilter(
-    DBusConnection* connection,
-    DBusMessage* message,
-    void* data) {
-  if (dbus_message_is_signal(message,
-                             DBUS_INTERFACE_LOCAL,
-                             kDisconnectedSignal)) {
-    // Abort when the connection is lost.
-    LOG(FATAL) << "D-Bus connection was disconnected. Aborting.";
-  }
-  return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
-}
-#endif
 
 // static
 DBusHandlerResult Bus::OnServiceOwnerChangedFilter(
