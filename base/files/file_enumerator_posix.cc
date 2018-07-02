536f6cb9217032dfd1d4cdbfc35b5d1c316cec27
Yunlian Jiang <yunlian@google.com>
2018-07-02 12:01:01 -0700

    libchrome: Remove usage of readdir_r()
    readdir_r() is deprecated, which causes build failures when using
    glibc-2.24 or newer headers.  This CL replaces usage of readdir_r()
    with readdir(). Part of it is a cherry-pick of
    http://crrev.com/4e33415240d5799
    
    BUG=chromium:859601
    TEST=emerge-samus libchrome with glibc 2.27
    
    Change-Id: If530eeb5faf7748b59fdd7a367a03ff02eaa95a1
    Reviewed-on: https://chromium-review.googlesource.com/1123101
    Commit-Ready: Yunlian Jiang <yunlian@chromium.org>
    Tested-by: Yunlian Jiang <yunlian@chromium.org>
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>

--- base/files/file_enumerator_posix.cc
+++ base/files/file_enumerator_posix.cc
@@ -131,9 +131,8 @@ bool FileEnumerator::ReadDirectory(std::vector<FileInfo>* entries,
          additional space for pathname may be needed
 #endif
 
-  struct dirent dent_buf;
   struct dirent* dent;
-  while (readdir_r(dir, &dent_buf, &dent) == 0 && dent) {
+  while ((dent = readdir(dir))) {
     FileInfo info;
     info.filename_ = FilePath(dent->d_name);
 
