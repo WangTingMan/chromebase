64062ae5f9882e20a61911965d8aa21296eae1a8
Qijiang Fan <fqj@google.com>
2020-02-17 13:18:55 +0900

    sync non-crypto with Chromium OS
    In preparation for upstream swap.
    
    Bug: chromium:915258
    Test: Presubmit
    Change-Id: Ib1ef7ed6975def5309c79c61a3012bc28a1a168c

---
+++ third_party/re2/src/re2/re2.h
@@ -0,0 +1,6 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Redirect to system header.
+#include <re2/re2.h>
