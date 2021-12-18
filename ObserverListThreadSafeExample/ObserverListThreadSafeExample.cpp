f7c0b4a7affcf3e8a7ec3f99848cbcdf68f25b0d
Grass <75142601@163.com>
2021-12-18 10:59:58 +0800

    Add Observer List Thread Safe Example
    Add a observer list thread safe example to show how
    to use ObserverListThreadSafe.

---
+++ ObserverListThreadSafeExample/ObserverListThreadSafeExample.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <functional>
+
+#include <base\system_monitor\system_monitor.h>
+#include <base\message_loop\message_loop.h>
+#include <base\observer_list_threadsafe.h>
+#include <base\memory\scoped_refptr.h>
+
+void Foo()
+{
+    std::cout << "Foo notified!\n";
+}
+
+int main()
+{
+    base::MessageLoop msgLoop;
+    base::RunLoop loop;
+
+    std::function<void( int, std::string )> fun = []( int x, std::string str )
+    {
+        std::cout << "here! " << x << " " << str << std::endl;
+    };
+
+    scoped_refptr<base::ObserverListThreadSafe<std::function<void( int, std::string )>>> list1
+    ( new base::ObserverListThreadSafe<std::function<void( int, std::string )>>() );
+    list1->AddObserver( &fun );
+
+    scoped_refptr<base::ObserverListThreadSafe<decltype( Foo )>> list2
+    ( new base::ObserverListThreadSafe<decltype( Foo )>() );
+    list2->AddObserver( &Foo );
+
+    list1->Notify( FROM_HERE, 23, "123" );
+    list1->Notify( FROM_HERE );
+    list2->Notify( FROM_HERE );
+    loop.Run();
+}
