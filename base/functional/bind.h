57f727b567b5eed80427cdb3b5526dbbb4a32f6b
Grace Cham <hscham@google.com>
2023-01-25 17:01:37 +0900

    libchrome: add base/functional/* headers
    For forward compatibility with r1093705 libchrome where
    base/{bind.h, callback.h, callback_forward.h, callback_helpers.h} are
    moved to base/functional/.
    This makes migration in CrOS easier on client packages shared with
    Android.
    
    Bug: 266161904
    Test: presubmit
    Change-Id: I7622eb821d5a7b97f37a725b0b860cebd0e99e9a

---
+++ base/functional/bind.h
@@ -0,0 +1,13 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// For forward compatibility: r1093705 libchrome moved base/bind.h to
+// base/functional/.
+
+#ifndef BASE_FUNCTIONAL_BIND_H_
+#define BASE_FUNCTIONAL_BIND_H_
+
+#include <base/bind.h>
+
+#endif  // BASE_FUNCTIONAL_BIND_H_
---
+++ base/functional/callback_helpers.h
@@ -0,0 +1,13 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// For forward compatibility: r1093705 libchrome moved base/callback_helpers.h
+// to base/functional/.
+
+#ifndef BASE_FUNCTIONAL_CALLBACK_HELPERS_H_
+#define BASE_FUNCTIONAL_CALLBACK_HELPERS_H_
+
+#include <base/callback_helpers.h>
+
+#endif  // BASE_FUNCTIONAL_CALLBACK_HELPERS_H_
